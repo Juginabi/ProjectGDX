f133c9e9047c42f7b6cc64ca0b6ae490af86ae65
Jukka Vatjus-Anttila <jukka.vatjus-anttila@cyberlightning.com>
2015-02-20 11:46:13 +0200

    Hull of the libGDX project

---
+++ android/src/com/juginabi/towerdefence/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.juginabi.towerdefence.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.juginabi.towerdefence.TowerDefence;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TowerDefence(), config);
+	}
+}
---
+++ desktop/src/com/juginabi/towerdefence/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.juginabi.towerdefence.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.juginabi.towerdefence.TowerDefence;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TowerDefence(), config);
+	}
+}
